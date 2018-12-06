fbcaa6413cd238d7ee785cf7e23524411ffea1d4
davidkelley <david.james.kelley@gmail.com>
2018-12-06 21:23:23 +0000

    test(styles/theme): implemented palette test

---
+++ styles/theme/src/palette/index.test.js
@@ -0,0 +1,28 @@
+import * as palette from '.';
+
+const names = ['accent', 'negative', 'neutral', 'positive', 'primary'];
+
+const hues = [200, 300, 400, 500, 600, 700, 800];
+
+describe('Palette', () => {
+  names.forEach(name => {
+    describe(`Color "${name}"`, () => {
+      it('exists', () => {
+        expect(palette[name]).toEqual(expect.any(Object));
+      });
+
+      hues.forEach(hue => {
+        describe(`Hue: "${hue}"`, () => {
+          it('is a valid rgb color', () => {
+            expect(palette[name][hue].displayable()).toBe(true);
+          });
+
+          it('stringifies correctly', () => {
+            const rgb = palette[name][hue].toString();
+            expect(rgb).toMatch(/^rgb\([0-9]{1,3}, [0-9]{1,3}, [0-9]{1,3}\)$/);
+          });
+        });
+      });
+    });
+  });
+});
